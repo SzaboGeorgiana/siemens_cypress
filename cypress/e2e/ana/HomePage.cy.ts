fc576d1f7ac433d24593c5e924d9a2ff280e4c8c
z004zy5f <Isarescu.Co.Anamaria@student.utcluj.ro>
2024-09-19 10:14:46 +0300

    Navbar

--- cypress/e2e/ana/HomePage.cy.ts
+++ cypress/e2e/ana/HomePage.cy.ts
@@ -1,1 +1,101 @@
 import { homePage } from "../../ui-manager/ana/pages/pages";
+
+beforeEach(() => {
+    cy.visit("/")
+  })
+
+  //navbar buttons
+
+  it("Home Button Test", () => {
+    homePage.homeButton().should("be.visible").click()
+    cy.url().should("eq", "https://ancabota09.wixsite.com/intern")
+  })
+
+  it("Explore Button Test", () => {
+    homePage.exploreButton().should("be.visible").click()
+    cy.url().should("eq", "https://ancabota09.wixsite.com/intern/explore")
+  })
+
+  it("Rooms Button Test", () => {
+    homePage.roomsButton().should("be.visible").click()
+    cy.url().should("eq", "https://ancabota09.wixsite.com/intern/rooms")
+  }) 
+
+  it("Contact Button Test", () => {
+    homePage.contactButton().should("be.visible").click()
+    cy.url().should("eq", "https://ancabota09.wixsite.com/intern/contact")
+  })
+
+  it("Book Now Button Test", () => {
+    homePage.contactButton().should("be.visible").click()
+    cy.url().should("eq", "https://ancabota09.wixsite.com/intern/booknow")
+  })
+
+  it("Page Title Button Test", () => {
+    homePage.pageTitleButton().should("be.visible").click()
+    cy.url().should("eq", "https://ancabota09.wixsite.com/intern/")
+  })
+
+  //footer social media buttons
+
+  it("Facebook Button Test", () => {
+    //homePage.facebookButton().should("have.prop", "target", "_blank").invoke('removeAttr', 'target', '_self').click({ force: true })
+    //should("exist").invoke("removeAttr", "target").click({ force: true })
+    //cy.url({ timeout: 10000 }).should("eq", "https://www.facebook.com/wix");
+    homePage.facebookButton()
+      .should("be.visible")
+      .invoke('attr', 'href')
+      .should('equal', 'http://www.facebook.com/wix')
+  })
+
+  it("X Button Test", () => {
+    homePage.xButton()
+      .should("be.visible")
+      .invoke('attr', 'href')
+      .should('equal', 'https://x.com/wix')
+  })
+
+  it("Pinterest Button Test", () => {
+    homePage.pinterestButton()
+      .should("be.visible")
+      .invoke('attr', 'href')
+      .should('equal', 'http://pinterest.com/wixcom/')
+  })
+
+  //footer buttons
+
+  it("Wix Page Button Test", () => {
+    // homePage.wixPageButton().invoke('removeAttr', 'target').click()
+    // cy.url({ timeout: 10000 }).should('equal', 'https://www.wix.com/?utm_campaign=vir_created_with')
+    // cy.go('back');
+    homePage.wixPageButton()
+      .should("be.visible")
+      .invoke('attr', 'href')
+      .should('equal', 'http://wix.com/?utm_campaign=vir_created_with');
+  })
+
+  it("Contact Mail Button Test", () => {
+    homePage.contactMailButton()
+      .should("be.visible")
+      .should('have.attr', 'href')
+      .should('contain', 'info@mysite.com');
+  })
+  
+  //Search Widget
+  it.only("Selector Check In Button Test", () => {
+    homePage.searchWidgetIframe()
+    .should('be.visible')
+    .should('not.be.empty')
+    .then(($iframe) => {
+        const $body = $iframe.contents().find('body')
+
+    cy.wrap($body)
+      .find('#check-in')
+      .click()
+    })
+
+    
+  })
+
+
+
--- cypress/ui-manager/ana/pages/pages.ts
+++ cypress/ui-manager/ana/pages/pages.ts
@@ -1,8 +1,23 @@
 export const homePage = {
+    //Navbar
     roomsButton: () => cy.get("#i6kl732v2label"),
     clickOnRoomsButton: () => cy.get("#i6kl732v2label").should("be.visible").click(),
-    exploreButton: () => cy.get("#i6kl732v1label")
-    
+    exploreButton: () => cy.get("#i6kl732v1label"),
+    contactButton: () => cy.get("#i6kl732v3label"),
+    bookNowButton: () => cy.get('[data-testid="linkElement"] > .l7_2fn'),
+    homeButton: () => cy.get("#i6kl732v0label"),
+    pageTitleButton: () => cy.get('.font_0 > .wixui-rich-text__text'),
+    //footer social media buttons
+    facebookButton: () => cy.get('#i0odz-i6rlbitx > a'),
+    xButton: () => cy.get('#i220sc-i6rlbitx > a'),
+    pinterestButton: () => cy.get('#i3175p-i6rlbitx > a'),
+    //footer buttons
+    wixPageButton: () => cy.get('#i71wwqnj > p:nth-child(2) > span > a'),
+    contactMailButton: () => cy.get('#i71ww6nk > p:nth-child(1) > a'),
+    //Search Widget
+    searchWidgetIframe: () => cy.get('#i6kppi75 > iframe'),
+    calendarCheckInButton: () => cy.get('#check-in'),
+
   }
   
   export const roomsPage = {
