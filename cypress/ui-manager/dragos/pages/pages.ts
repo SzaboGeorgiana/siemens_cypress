b3e715a5dd82c32cbd06e06e063dbf68ba88efc5
Dragosh7 <dragos.04@aol.com>
2024-09-25 12:35:53 +0300

    added chat tests

--- cypress/ui-manager/dragos/pages/pages.ts
+++ cypress/ui-manager/dragos/pages/pages.ts
@@ -111,3 +111,22 @@ export const roomsURL = "https://ancabota09.wixsite.com/intern/rooms";
 
     submitFeedback: () => cy.get('#comp-jxbsa1dm > [data-testid="inline-content"] > [data-testid="mesh-container-content"] > [data-testid="richTextElement"] > .font_8 > .color_15')
   }
+
+  export const chatFrame = {
+    iframeSelector: '#comp-jr4sqg2g > .nKphmK',
+    chatBtn: '#minimized-chat',
+    chat2: 'div.kBk8- button[data-hook="minimized-chat"]',
+    chatTextArea: 'textarea[aria-label="Type your message. Hit enter to submit."]',
+    sendBtn: 'button[data-hook="send-button"]',
+    chatMessages: 'div[data-hook="chat-message"]',
+
+    chatForm: 'form.zd18O',
+    inputName: 'input#name',
+    inputEmail: 'input#email',
+    inputMessage: 'textarea#message',
+    chatEmailError: '#email-error',
+    chatNameError: '#name-error',
+    submitForm: 'button[data-hook="lcf-submit"]',
+    formFeedback: 'div[data-hook="lcf-thank-you"]'
+    
+  }
